419cd897abe2d8bd61fa37672f85ccdeba2dfa47
Marcelin NKOMO <marcelin.arsene.nkomo@gmail.com>
2025-05-14 12:05:36 +0200

    init repository

---
+++ src/main/java/com/bpifrance/beneficiaryapi/BeneficiaryapiApplication.java
@@ -0,0 +1,13 @@
+package com.bpifrance.beneficiaryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeneficiaryapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeneficiaryapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bpifrance/beneficiaryapi/BeneficiaryapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpifrance.beneficiaryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeneficiaryapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
